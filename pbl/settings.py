779d6c4e4899094997f6780561c1d9820362d9f4
AryanKadu <aryanpkadu30@gmail.com>
2025-08-02 13:14:10 +0530

    fourth commit

--- pbl/settings.py
+++ pbl/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-d5e+3%pk68^e&(1bu1!l*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,strideup.onrender.com').split(',')
 
 # CSRF settings for Render
 CSRF_TRUSTED_ORIGINS = [
